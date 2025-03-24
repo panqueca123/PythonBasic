dd0337096c30be9446dec4facb7f245f9fdb51b8
panqueca123 <166817782+panqueca123@users.noreply.github.com>
2025-03-24 17:14:16 -0300

    Create 004.py

---
+++ py/004.py
@@ -0,0 +1,11 @@
+def inicio():
+    raio = 0 
+    area = 0
+
+
+    print("Digite o raio do circulo")
+    raio = int(input())
+    area = 3.1416* (raio ** 2)
+    print("Área do circulo é:", area)
+
+inicio()
